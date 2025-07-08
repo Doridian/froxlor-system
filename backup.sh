11ce3506c970e91500fd75fbf4262eb9760a25b2
Doridian <git@doridian.net>
2025-07-08 16:52:53 -0700

    fix

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+rm -rf /mnt/backups/mysql
 mkdir -p /mnt/backups/webs /mnt/backups/mail /mnt/backups/mysql
 chmod 700 /mnt/backups/webs /mnt/backups/mail /mnt/backups/mysql
 
 rsync -trv --delete /var/customers/webs/ /mnt/backups/webs/
 rsync -trv --delete /var/customers/mail/ /mnt/backups/mail/
 
-mariadb-backup --backup --innobackupex --target-dir /mnt/backups/mysql
-mariadb-backup --prepare --target-dir /mnt/backups/mysql
+mariadb-backup --backup --target-dir /mnt/backups/mysql
