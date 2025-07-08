1b80ffce621f59bd187ff6f6c9ee323281074c25
Doridian <git@doridian.net>
2025-07-08 16:53:26 -0700

    fix

--- backup.sh
+++ backup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
+echo 'Starting backup process...'
 rm -rf /mnt/backups/mysql
 mkdir -p /mnt/backups/webs /mnt/backups/mail /mnt/backups/mysql
 chmod 700 /mnt/backups/webs /mnt/backups/mail /mnt/backups/mysql
 
+echo 'Backing up websites...'
 rsync -trv --delete /var/customers/webs/ /mnt/backups/webs/
+
+echo 'Backing up emails...'
 rsync -trv --delete /var/customers/mail/ /mnt/backups/mail/
 
+echo 'Backing up MySQL databases...'
 mariadb-backup --backup --target-dir /mnt/backups/mysql
