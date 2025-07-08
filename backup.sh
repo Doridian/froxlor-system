50c1ce4d47715f925961b9a7204f46966976268d
Doridian <git@doridian.net>
2025-07-08 16:39:39 -0700

    chmod

--- backup.sh
+++ backup.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 mkdir -p /mnt/backups/webs /mnt/backups/mail
 chmod 700 /mnt/backups/webs /mnt/backups/mail
 
-rsync -rv --delete /var/customers/webs/ /mnt/backups/webs/
-rsync -rv --delete /var/customers/mail/ /mnt/backups/mail/
+rsync -trv --delete /var/customers/webs/ /mnt/backups/webs/
+rsync -trv --delete /var/customers/mail/ /mnt/backups/mail/
