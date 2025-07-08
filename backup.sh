37059798e28874c4662cb1aa276cc148fe157f68
Doridian <git@doridian.net>
2025-07-08 16:50:11 -0700

    mh

--- backup.sh
+++ backup.sh
@@ -7,8 +7,5 @@ chmod 700 /mnt/backups/webs /mnt/backups/mail /mnt/backups/mysql
 rsync -trv --delete /var/customers/webs/ /mnt/backups/webs/
 rsync -trv --delete /var/customers/mail/ /mnt/backups/mail/
 
-DATABASES="$(mysql --batch -e 'SHOW DATABASES;' | grep -v '^Database\|information_schema\|performance_schema\|sys$')"
-
-while IFS= read -r dbname; do
-    mysqldump --single-transaction --routines --triggers "$dbname" | gzip > "/mnt/backups/mysql/$dbname.sql.gz"
-done <<< "$DATABASES"
+mariadb-backup --backup --innobackupex --target-dir /mnt/backups/mysql
+mariadb-backup --prepare --target-dir /mnt/backups/mysql
