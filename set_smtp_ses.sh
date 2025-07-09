fc4ea970aa1a9aad5e77e603cd657b11f6797020
Doridian <git@doridian.net>
2025-07-08 17:56:03 -0700

    safety

--- set_smtp_ses.sh
+++ set_smtp_ses.sh
@@ -1,6 +1,8 @@
 # /etc/postfix/sasl_passwd should contain the following line:
 # [email-smtp.eu-west-1.amazonaws.com]:587 ACCESS_KEY:SECRET_KEY
 
+chmod 600 /etc/postfix/sasl_passwd
+chown root:root /etc/postfix/sasl_passwd
 postmap /etc/postfix/sasl_passwd
 chmod 640 /etc/postfix/sasl_passwd.db
 chown root:postfix /etc/postfix/sasl_passwd.db
