0175e93a295a55629d64b5a14010c57e57b03886
Doridian <git@doridian.net>
2025-07-08 22:09:26 -0700

    shared

---
+++ shared.php
@@ -0,0 +1,32 @@
+#!/usr/bin/env php
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+require_once '/var/www/html/froxlor/lib/userdata.inc.php';
+
+$db = new mysqli(
+    $sql['host'],
+    $sql['user'],
+    $sql['password'],
+    $sql['db'],
+) or die('Database connection error '. mysqli_connect_error());
+
+function get_setting($group, $name) {
+    global $db;
+    $stmt = $db->prepare('SELECT value FROM panel_settings WHERE settinggroup = ? AND varname = ? LIMIT 1');
+    $stmt->bind_param('ss', $group, $name);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows === 0) {
+        die("Setting $group.$name not found in panel_settings, please set it in the Froxlor settings.");
+    }
+    $row = $result->fetch_assoc();
+    $value = $row['value'];
+    if (empty($value)) {
+        die("Setting $group.$name is empty, please set it in the Froxlor settings.");
+    }
+    $stmt->close();
+    return $value;
+}
--- update.php
+++ update.php
@@ -1,39 +1,11 @@
 #!/usr/bin/env php
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
-require_once '/var/www/html/froxlor/lib/userdata.inc.php';
-
-$fqdn = strtolower(trim(shell_exec('hostname -f')));
-
-$db = new mysqli(
-    $sql['host'],
-    $sql['user'],
-    $sql['password'],
-    $sql['db'],
-) or die('Database connection error '. mysqli_connect_error());
-
-function get_setting($group, $name) {
-    global $db;
-    $stmt = $db->prepare('SELECT value FROM panel_settings WHERE settinggroup = ? AND varname = ? LIMIT 1');
-    $stmt->bind_param('ss', $group, $name);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if ($result->num_rows === 0) {
-        die("Setting $group.$name not found in panel_settings, please set it in the Froxlor settings.");
-    }
-    $row = $result->fetch_assoc();
-    $value = $row['value'];
-    if (empty($value)) {
-        die("Setting $group.$name is empty, please set it in the Froxlor settings.");
-    }
-    $stmt->close();
-    return $value;
-}
+require_once 'shared.php';
 
 $ssl_dir = rtrim(get_setting('system', 'customer_ssl_path'), '/') . '/';
+
+$fqdn = strtolower(trim(shell_exec('hostname -f')));
 
 $ips_res = $db->query('SELECT DISTINCT ip FROM panel_ipsandports;');
 $ips = [];
