d0b891445fc3bdadf19cf3c138a8aa920c8731a7
Doridian <git@doridian.net>
2025-07-08 23:42:15 -0700

    crontabs

--- backup.sh
+++ backup.sh
@@ -3,14 +3,17 @@ set -euo pipefail
 
 echo 'Starting backup process...'
 rm -rf /mnt/backups/mysql
-mkdir -p /mnt/backups/webs /mnt/backups/mail /mnt/backups/mysql
-chmod 700 /mnt/backups/webs /mnt/backups/mail /mnt/backups/mysql
+mkdir -p /mnt/backups/webs /mnt/backups/mail /mnt/backups/mysql /mnt/backups/crontabs
+chmod 700 /mnt/backups/webs /mnt/backups/mail /mnt/backups/mysql /mnt/backups/crontabs
 
 echo 'Backing up websites...'
 rsync -trv --delete /var/customers/webs/ /mnt/backups/webs/
 
 echo 'Backing up emails...'
 rsync -trv --delete /var/customers/mail/ /mnt/backups/mail/
+
+echo 'Backing up crontabs...'
+rsync -trv --delete /var/spool/cron/crontabs/ /mnt/backups/crontabs/
 
 echo 'Backing up MySQL databases...'
 mariadb-backup --backup --target-dir /mnt/backups/mysql
