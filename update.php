2fdae31da1a9e2cb8355f227748b9e491d68653b
Doridian <git@doridian.net>
2025-07-09 11:33:16 -0700

    Helper func for pureftpd

--- update.php
+++ update.php
@@ -20,6 +20,25 @@ $pureftpd_tls->writeln('case "$CERTD_SNI_NAME" in');
 $dovecot_tls->writeln("ssl_cert = <$fqdn_fullchain_file");
 $dovecot_tls->writeln("ssl_key = <$fqdn_key_file");
 
+function write_pureftpd_tls($domains, $fullchain_file, $key_file) {
+    global $pureftpd_tls;
+
+    if (count($domains) === 1 && $domains[0] === '*') {
+        $domains_str = '*';
+    } else {
+        $domains_escaped = array_map('escapeshellarg', $domains);
+        $domains_str = implode('|', $domains_escaped);
+    }
+
+    $fullchain_escaped = escapeshellarg("cert_file:$fullchain_file");
+    $key_escaped = escapeshellarg("key_file:$key_file");
+
+    $pureftpd_tls->writeln("  $domains_str)");
+    $pureftpd_tls->writeln("    echo $fullchain_escaped");
+    $pureftpd_tls->writeln("    echo $key_escaped");
+    $pureftpd_tls->writeln('    ;;');
+}
+
 $cert_res = $db->query('SELECT d.domain AS domain, s.ssl_cert_file AS ssl_cert_file FROM panel_domains d, domain_ssl_settings s WHERE d.id = s.domainid;');
 while ($cert_row = $cert_res->fetch_assoc()) {
     $domain_raw = $cert_row['domain'];
@@ -88,18 +107,12 @@ while ($cert_row = $cert_res->fetch_assoc()) {
         $dovecot_tls->writeln('}');
     }
 
-    $pureftpd_tls->writeln("  '" . implode("'|'", $domains) . "')");
-    $pureftpd_tls->writeln("    echo 'cert_file:$fullchain_file'");
-    $pureftpd_tls->writeln("    echo 'key_file:$key_file'");
-    $pureftpd_tls->writeln('    ;;');
+    write_pureftpd_tls($domains, $fullchain_file, $key_file);
 }
 
 unset($cert_row, $cert_res, $domain_raw, $fullchain_file, $key_file, $cert_data, $domains);
 
-$pureftpd_tls->writeln('  *)');
-$pureftpd_tls->writeln("    echo 'cert_file:" . $fqdn_fullchain_file . "'");
-$pureftpd_tls->writeln("    echo 'key_file:" . $fqdn_key_file . "'");
-$pureftpd_tls->writeln("    ;;");
+write_pureftpd_tls(['*'], $fqdn_fullchain_file, $fqdn_key_file);
 $pureftpd_tls->writeln('esac');
 $pureftpd_tls->writeln("echo 'end'");
 
