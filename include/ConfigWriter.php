adda23323b94cd47c9098f33ed4b6fc80bf48805
Doridian <git@doridian.net>
2025-07-09 21:31:16 -0700

    strict_Types

--- include/ConfigWriter.php
+++ include/ConfigWriter.php
@@ -1,4 +1,5 @@
 <?php
+declare (strict_types=1);
 
 require_once 'shared.php';
 require_once 'SafeTempFile.php';
--- include/ITLSConfigHolder.php
+++ include/ITLSConfigHolder.php
@@ -1,4 +1,5 @@
 <?php
+declare (strict_types=1);
 
 interface ITLSConfigHolder {
     public function getConfigs(): array;
