6daa8ab764dd8573d54f6ff68087bc719c0dad16
Doridian <git@doridian.net>
2025-07-10 09:48:45 -0700

    backup etc

--- backup.sh
+++ backup.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 set -euo pipefail
 
+mkdir_safe() {
+    mkdir -p "$1"
+    chmod 700 "$1"
+}
+
+rsync_cmd() {
+    rsync -trv --delete "$1" "$2"
+}
+
 echo 'Starting backup process...'
-rm -rf /mnt/backups/mysql
-mkdir -p /mnt/backups/webs /mnt/backups/mail /mnt/backups/mysql /mnt/backups/crontabs
-chmod 700 /mnt/backups/webs /mnt/backups/mail /mnt/backups/mysql /mnt/backups/crontabs
+
+echo 'Backing up system files...'
+mkdir_safe /mnt/backups/system
+rsync_cmd /etc/ /mnt/backups/system/etc/
 
 echo 'Backing up websites...'
-rsync -trv --delete /var/customers/webs/ /mnt/backups/webs/
+mkdir_safe /mnt/backups/webs
+rsync_cmd /var/customers/webs/ /mnt/backups/webs/
 
 echo 'Backing up emails...'
-rsync -trv --delete /var/customers/mail/ /mnt/backups/mail/
+mkdir_safe /mnt/backups/mail
+rsync_cmd /var/customers/mail/ /mnt/backups/mail/
 
 echo 'Backing up crontabs...'
-rsync -trv --delete /var/spool/cron/crontabs/ /mnt/backups/crontabs/
+mkdir_safe /mnt/backups/crontabs
+rsync_cmd /var/spool/cron/crontabs/ /mnt/backups/crontabs/
 
 echo 'Backing up MySQL databases...'
+rm -rf /mnt/backups/mysql
+mkdir_safe /mnt/backups/mysql
 mariadb-backup --backup --target-dir /mnt/backups/mysql
 mariadb-backup --prepare --target-dir /mnt/backups/mysql
