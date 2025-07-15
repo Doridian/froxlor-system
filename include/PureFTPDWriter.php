a4d84f63d49e54f4d4a53b92cda5150979cb08b3
Doridian <git@doridian.net>
2025-07-15 12:23:40 -0700

    certfile

--- include/PureFTPDWriter.php
+++ include/PureFTPDWriter.php
@@ -3,6 +3,8 @@ declare (strict_types=1);
 
 require_once 'ConfigWriter.php';
 
+define('PURE_FTPD_CERTFILE', '/etc/pure-ftpd/conf/CertFile');
+
 class PureFTPDWriter extends ConfigWriter {
     public function __construct() {
         parent::__construct('/etc/pure-ftpd/tls_server_sni_maps');
@@ -20,8 +22,15 @@ class PureFTPDWriter extends ConfigWriter {
         verboseRun('postmap /etc/pure-ftpd/tls_server_sni_maps');
 
         if ($defaultConfig) {
-            file_put_contents('/etc/pure-ftpd/conf/CertFile', $defaultConfig->fullChainFile . ',' .
-                                                                $defaultConfig->keyFile . PHP_EOL);
+            $defaultData = $defaultConfig->fullChainFile . ',' .
+                           $defaultConfig->keyFile . PHP_EOL;
+            if (file_get_contents(PURE_FTPD_CERTFILE) !== $defaultData) {
+                file_put_contents(PURE_FTPD_CERTFILE, $defaultData);
+                verboseRun('systemctl restart pure-ftpd-mysql');
+            }
+        } else if (file_exists(PURE_FTPD_CERTFILE)) {
+            unlink(PURE_FTPD_CERTFILE);
+            verboseRun('systemctl restart pure-ftpd-mysql');
         }
     }
 }
