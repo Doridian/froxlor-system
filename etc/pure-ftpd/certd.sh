e70564a45d761d5a10339fc67f5af19e4867d09b
Doridian <git@doridian.net>
2025-07-15 12:30:40 -0700

    sutff

--- etc/pure-ftpd/certd.sh
+++ etc/pure-ftpd/certd.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-set -e
+postmap -F -q "$CERTD_SNI_NAME" /etc/pure-ftpd/tls_server_sni_maps && exit 0
 
-defconf() {
-    echo 'action:default'
-    echo 'end'
-}
-
-postmap -F -q "$CERTD_SNI_NAME" /etc/pure-ftpd/tls_server_sni_maps || defconf
+echo 'action:default'
+echo 'end'
