0f4584a2c1ca4cfd6fc39a7af82a63a8c880d448
Doridian <git@doridian.net>
2025-08-06 16:20:25 -0700

    aaaa

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ apt-get -y install unattended-upgrades restic screen htop phpmyadmin sudo mariad
 echo 'Rendering system configuration files...'
 rm -rf build && mkdir -p build
 
-find etc -type d -exec mkdir -p "build/"{} \;
-find etc -type f -exec /bin/sh -c 'envsubst \$INSTALLDIR < "$1" > "build/$1"' -- {} \;
+find etc -type d -print0 | xargs -0 -n1 mkdir -p "build/$1"
+find etc -type f -print0 | xargs -0 -n1 sed "s~__INSTALLDIR__~${INSTALLDIR}~g" "build/$1"
 
 cp -rv build/etc/* /etc/
 
