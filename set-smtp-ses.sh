dabbb7f135d3e0844ef5770807b4f27afef5f86b
Doridian <git@doridian.net>
2025-08-23 00:29:19 -0700

    unify quotes

--- set-smtp-ses.sh
+++ set-smtp-ses.sh
@@ -13,7 +13,7 @@ chown root:postfix /etc/postfix/sasl_passwd.db
 
 postconf 'smtp_sasl_password_maps=hash:/etc/postfix/sasl_passwd'
 postconf 'smtp_sasl_security_options=noanonymous'
-postconf "relayhost=[email-smtp.eu-west-1.amazonaws.com]:587"
-postconf "smtp_sasl_auth_enable=yes"
+postconf 'relayhost=[email-smtp.eu-west-1.amazonaws.com]:587'
+postconf 'smtp_sasl_auth_enable=yes'
 
 systemctl restart postfix
