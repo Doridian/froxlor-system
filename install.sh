03ef4d8a58798cfb7417ded65f104bb2e9822cb8
Doridian <git@doridian.net>
2026-06-20 15:25:54 -0700

    Allow self-signed for mailonly

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@ export INSTALLDIR="$(pwd)"
 
 apt-get -y update
 apt-get -y install unattended-upgrades restic screen htop phpmyadmin sudo mariadb-backup ncdu git rsync opendkim
-mkdir -p /etc/opendkim/keys
+mkdir -p /etc/opendkim/keys /var/lib/opendkim
 chmod 700 /etc/opendkim/keys
-chown opendkim:opendkim /etc/opendkim/keys
+chown opendkim:opendkim /etc/opendkim/keys /var/lib/opendkim
+usermod -a -G opendkim postfix
 
 echo 'Rendering system configuration files...'
 rm -rf build && mkdir -p build
@@ -27,7 +28,7 @@ fi
 
 rm -rf build
 
-postconf -e tls_server_sni_maps=proxy:hash:/etc/postfix/tls_server_sni_maps inet_protocols=all
+postconf -e tls_server_sni_maps=proxy:hash:/etc/postfix/tls_server_sni_maps inet_protocols=all smtpd_milters=unix:/var/lib/opendkim/opendkim.sock 'non_smtpd_milters=$smtpd_milters' milter_default_action=accept
 postconf -X smtpd_tls_CAfile smtpd_tls_cert_file smtpd_tls_keyFile
 
 echo 'Running update...'
